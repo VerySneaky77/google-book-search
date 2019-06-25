a3c8a39dd2b86f871af08fa52bbe2c0f7ddbfba8
VerySneaky77 <carlquin77@gmail.com>
2019-06-24 23:35:20 -0700

    Updated readme, build created

---
+++ client/build/precache-manifest.1282adf3bc47d17df192d6dd924a88c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6dbf965ce8459b8f8eaa",
+    "url": "/static/js/main.64fa6722.chunk.js"
+  },
+  {
+    "revision": "f8bad00d18e2e689091f",
+    "url": "/static/js/2.57fe285c.chunk.js"
+  },
+  {
+    "revision": "6dbf965ce8459b8f8eaa",
+    "url": "/static/css/main.daefee5b.chunk.css"
+  },
+  {
+    "revision": "220b523f1c19850d1c4cb475eaa1eb72",
+    "url": "/index.html"
+  }
+];
